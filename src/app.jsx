cf4cff54bf4e9f0929d3f054acf091910a41e27b
kishuu0053 <kishwerzia131@gmail.com>
2026-06-13 12:46:09 +0500

    Fix import casing

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,24 @@
-import{ BrowserRouter,Routes,Route } from "react-router-dom"
-import React from 'react'
-import Home from './pages/Home'
-import Navbar from './components/shared/Navbar'
-import Footer from './components/shared/Footer'
-import About from './pages/About'
-import Serviceoffer from './components/ui/Serviceoffer'
-// import { Features } from 'tailwindcss'
-import Services from './pages/Services'
-import Contact from './pages/Contact'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/shared/Navbar";
+import Footer from "./components/shared/Footer";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-    <Routes>
-      < Route path="/" element={<Home/>} />
-      < Route path="about" element={<About/>} />
-      < Route path="service" element={<Services/>} />
-      < Route path="contact" element={<Contact/>} />
-    
-  </Routes>
-       
-          
-          <Footer/>
-            
-     
-      
-     
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
